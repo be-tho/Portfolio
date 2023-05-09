ff9237b9331f2179c1750f318809e4403aa77b52
beto <56642267+be-tho@users.noreply.github.com>
2023-05-09 00:35:05 -0300

    proyecto laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Portfolio de albert villarroel</title>
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<header class="container p-8">
+	<nav>
+		Este es el menu
+	</nav>
+</header>
+<section class="container bg-blue-300 p-8">
+	<h1 class="text-blue-800">Hola mundo!</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti distinctio ea explicabo laboriosam laborum neque numquam quis tempore veniam. Animi et explicabo iusto modi neque possimus quibusdam quisquam repudiandae tempora.</p>
+</section>
+</body>
+</html>
